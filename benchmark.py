d1e51280149b0279d826bc40fb68532221dc79a0
yunlanli <chnsidney@gmail.com>
2022-12-04 12:31:10 -0500

    initial script to benchmark p4 with different bucket width

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,42 @@
+import argparse
+import numpy as np
+import pickle
 import sys
 from typing import Any
 
-from main import parser
+from main import parser as ameoba_parser
 from amoeba_game import AmoebaGame
 
+
+# -----------------------------------------------------------------------------
+# 	Parser
+# -----------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+	"--out",
+	"-o",
+	type=str,
+	default='res/benchmark/default.p',
+	help="file path to store benchmark results as a picke file"
+)
+
+parser.add_argument(
+	"--seed",
+	"-s",
+	type=int,
+	default=5123467,
+	help="integer to seed np.random for generating seeds for simulator"
+)
+
+parser.add_argument(
+	"--player",
+	"-p",
+	type=int,
+	default=4,
+	help="player to benchmark"
+)
 
 # -----------------------------------------------------------------------------
 # 	Helpers
@@ -25,7 +58,7 @@ class DummyFile(object):
 
 class GameConfig:
 
-	base_args = ['-ng', '-l', '500', '-p', '4', '--no_vid']
+	base_args = ['-ng', '--no_vid', '-l', '500']
 
 	def _add_opt(self, opt: str, val: Any):
 		self.base_args.extend([opt, str(val)])
@@ -47,6 +80,10 @@ class GameConfig:
 		self._add_opt('-d', density)
 		return self
 
+	def add_seed(self, seed: int):
+		self._add_opt('-s', seed)
+		return self
+
 	def get_args(self) -> list:
 		return self.base_args
 
@@ -55,7 +92,8 @@ def create_config(
 	player: int,
 	size: int,
 	density: float,
-	metabolism: float
+	metabolism: float,
+	seed: int
 ) -> GameConfig:
 	return (
 		GameConfig()
@@ -63,6 +101,7 @@ def create_config(
 			.add_size(size)
 			.add_density(density)
 			.add_metabolism(metabolism)
+			.add_seed(seed)
 	)
 
 
@@ -83,7 +122,7 @@ def run(config: GameConfig) -> tuple[bool, int, float]:
 	save_stdout = sys.stdout
 	sys.stdout = DummyFile()
 
-	args = parser.parse_args(config.get_args())
+	args = ameoba_parser.parse_args(config.get_args())
 	game = AmoebaGame(args)
 
 	ok = game.goal_reached
@@ -101,11 +140,48 @@ def run(config: GameConfig) -> tuple[bool, int, float]:
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-	test_config = create_config(4, 5, 0.2, 1)
+	args = parser.parse_args()
 
-	ok, turns, ts = run(test_config)
-	print("{} - {} turns in {:.2f} second(s)".format(
-		"ok" if ok else "failed",
-		turns,
-		ts
-	))
+	np.random.seed(args.seed)
+	player = args.player
+	size = 5
+	metabolism = 1
+	densities = np.linspace(0.05, 0.95, num=10)
+	seeds = np.random.randint(0, high=sys.maxsize, size=3)
+
+	opts = [
+		(player, size, density, metabolism, seed)
+		for density in densities for seed in seeds
+	]
+
+	out = []
+	for opt in opts:
+		print(
+			"running p={}, A={}, density={:.2f}, metabolism={:.2f}, seed={}"
+			.format(*opt)
+		)
+		config = create_config(*opt)
+
+		ok, turns, ts = run(config)
+		print("{} - {} turns in {:.2f} second(s)".format(
+			"ok" if ok else "failed",
+			turns,
+			ts
+		))
+
+		run_result = [*opt, ok, turns, ts]
+		out.append(run_result)
+
+	print("\n---\nresults:\n")
+	print(
+		"%-8s %-3s %-9s %-12s %-22s %-10s %-7s %-6s" %
+		("player", "A", "density", "metabolism", "seed", "ok", "turns", "time")
+	)
+
+	for res in out:
+		print(
+			"%-8d %-3d %-9.2f %-12.2f %-22d %-10r %-7d %-6.2f" %
+			tuple(res)
+		)
+	with open(args.out, "wb") as f:
+		pickle.dump(out, f)
