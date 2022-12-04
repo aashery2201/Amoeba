fb5fc071c74b97205d1b068fbe8d47dd704916ba
yunlanli <chnsidney@gmail.com>
2022-12-04 10:25:28 -0500

    add run trial helper, collect game results

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,23 @@
+import sys
 from typing import Any
 
 from main import parser
 from amoeba_game import AmoebaGame
 
 
+# Suppress stdout
+# https://stackoverflow.com/questions/2828953/silence-the-stdout-of-a-function-in-python-without-trashing-sys-stdout-and-resto
+class DummyFile(object):
+    def write(self, x):
+        pass
+
+    def flush(self):
+        pass
+
+
 class GameConfig:
 
-	base_args = ['-l', '500', '-p', '4', '--no_vid']
+	base_args = ['-ng', '-l', '500', '-p', '4', '--no_vid']
 
 	def _add_opt(self, opt: str, val: Any):
 		self.base_args.extend([opt, str(val)])
@@ -32,13 +43,44 @@ class GameConfig:
 		return self.base_args
 
 
-test_config = GameConfig()
-(test_config
-	.add_player(4)
-	.add_metabolism(1)
-	.add_size(5)
-	.add_density(0.2)
-)
+def run(config: GameConfig) -> tuple[bool, int, float]:
+	"""Runs the simulator for the given game configuration, and returns
+	the results.
+	
+	Returns:
+	  ok: True if ameoba reached the goal size, otherwise False.
+	  turns: # turns taken to complete the game or tried but failed.
+	  ts: cpu time in seconds taken for the game.
+	"""
+	# suppress stdout
+	save_stdout = sys.stdout
+	sys.stdout = DummyFile()
 
-args = parser.parse_args(test_config.get_args())
-AmoebaGame(args)
+	args = parser.parse_args(test_config.get_args())
+	game = AmoebaGame(args)
+
+	ok = game.goal_reached
+	turns = game.turns
+	ts = game.end_time - game.start_time
+
+	# restore stdout
+	sys.stdout = save_stdout
+
+	return ok, turns, ts
+
+
+if __name__ == "__main__":
+	test_config = GameConfig()
+	(test_config
+		.add_player(4)
+		.add_metabolism(1)
+		.add_size(5)
+		.add_density(0.2)
+	)
+
+	ok, turns, ts = run(test_config)
+	print("{} - {} turns in {:.2f} second(s)".format(
+		"ok" if ok else "failed",
+		turns,
+		ts
+	))
